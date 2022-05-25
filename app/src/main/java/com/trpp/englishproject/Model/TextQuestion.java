d1a660b12e1fe1965546a10ba56c2a385bfb7a98
Егор Зотов <90198869+kwazr@users.noreply.github.com>
2022-05-25 19:43:28 +0300

    Update text

--- app/src/main/java/com/trpp/englishproject/Model/TextQuestion.java
+++ app/src/main/java/com/trpp/englishproject/Model/TextQuestion.java
@@ -8,23 +8,28 @@ public class TextQuestion {
     public TextQuestion() {
     }
 
+    
     public TextQuestion(String ans, String q) {
         this.q = q;
         this.ans = ans;
     }
 
+    
     public String getAns() {
         return ans;
     }
 
+    
     public void setAns(String ans) {
         this.ans = ans;
     }
 
+    
     public String getQ() {
         return q;
     }
 
+    
     public void setQ(String q) {
         this.q = q;
     }
